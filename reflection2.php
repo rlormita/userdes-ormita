525b8ab14d9de88ac1ba3fd9e33f087cdc9299ef
Rommel Ormita <52182399+rlormita@users.noreply.github.com>
2019-08-03 00:00:55 +0800

    lab 2 web app

---
+++ reflection2.php
@@ -0,0 +1,113 @@
+<?php header( 'Location: /index.html' ) ;  ?>
+<html>
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Reflection #2</title>
+
+  <!-- Custom fonts for this theme -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+  <!-- Theme CSS -->
+  <link href="css/freelancer.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">R.L.O</a>
+      <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="co llapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.html">Home</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
+    <header style="background-image:url(img/original.gif)" class="masthead bg-primary text-white text-center" >
+	<div class="container d-flex align-items-center flex-column">
+	
+	<!-- Masthead Avatar Image -->
+	
+	<h1 class="masthead-heading text-uppercase mb-0">Reflection #1</h1>
+	<br>
+	<br>
+	<br>
+	<img class="rounded-corner" src="img/reflection2.jpg" align="center" width="700" height="400" alt="">
+	<br>
+
+	<!-- Masthead Heading -->
+	<!-- Icon Divider -->
+	<div class="divider-custom divider-light">
+		<div class="divider-custom-line"></div>
+		<div class="divider-custom-icon">
+		<i class="fas fa-star"></i>
+		</div>
+		<div class="divider-custom-line"></div>
+	</div>
+
+	<!-- Masthead Subheading -->
+
+	</div>
+	<section class="page-section portfolio" id="portfolio">
+    <div class="container">
+        <p  align="left">This week, Ms. Jen introduced the topic of Needfinding. I thought that it was the “defining the problem” in one of the research processes but its not. It is after the defining the problem. I did not know about this word before but when Ms. Jen taught us about Needfinding, watched entertaining videos related to it made me realize that it is really important to solve problems. It would definitely help you in creating solutions. It is new learning for me and a new technique to generate ideas.
+		</p>
+		<p  align="left">
+		One of my groupmates told that the developers before, were so lucky because there are ideas that doesn’t made an application yet. But today, in creating an application, it was hard to generate an idea or concept when you know that there are many applications that were now created and solves the problems that you thought of.  But Needfindings guide us to brainstorm what application that we wanted to do, and the problems we want to solve.
+		</p>
+		<p align="left">
+		When we were in the Microsoft Experience Room, I also learned that we need to be in the user’s shoes in order to imagine how our application works. Also, I liked that Ms. Jen are telling the plotholes of each application idea and gives us suggestions to improve on it. In making an application, it needs to be more specific and detailed. It also has a certain goal to give its service to the users. You need to think that “does our application really solve the problem?”.
+		</p>
+	</div>
+	</section>
+  </header>
+  <footer class="footer text-center">
+    <div class="container">
+      <div class="column">
+		
+        <!-- Footer Social Icons -->
+        <div class="col-lg-4 mb-5 mb-lg-0">
+          <h4 class="text-uppercase mb-4">Around the Web</h4>
+          <a class="btn btn-outline-light btn-social mx-1" href="https://b-m.facebook.com/rmlrmt">
+            <i class="fab fa-fw fa-facebook-f"></i>
+          </a>
+          <a class="btn btn-outline-light btn-social mx-1" href="https://twitter.com/rmlrmt">
+            <i class="fab fa-fw fa-twitter"></i>
+          </a>
+          <a class="btn btn-outline-light btn-social mx-1" href="https://ph.linkedin.com/in/rommel-o-29a6ba111">
+            <i class="fab fa-fw fa-linkedin-in"></i>
+          </a>
+          <a class="btn btn-outline-light btn-social mx-1" href="https://www.instagram.com/rmlrmt/">
+            <i class="fab fa-fw fa-dribbble"></i>
+          </a>
+        </div>
+		
+      </div>
+    </div>
+  </footer>
+
+  <!-- Copyright Section -->
+  <section class="copyright py-4 text-center text-white">
+    <div class="container">
+	  <img src="img/mew.gif" width="50" height="50">
+      <small>Copyright &copy; labActivity2 2019</small>
+    </div>
+  </section>
+
+</html>
---
+++ reflection3.php
@@ -0,0 +1,113 @@
+<?php header( 'Location: /index.html' ) ;  ?>
+<html>
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Reflection #3</title>
+
+  <!-- Custom fonts for this theme -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+  <!-- Theme CSS -->
+  <link href="css/freelancer.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">R.L.O</a>
+      <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="co llapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.html">Home</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
+    <header style="background-image:url(img/original.gif)" class="masthead bg-primary text-white text-center" >
+	<div class="container d-flex align-items-center flex-column">
+	
+	<!-- Masthead Avatar Image -->
+	
+	<h1 class="masthead-heading text-uppercase mb-0">Reflection #3</h1>
+	<br>
+	<br>
+	<br>
+	<img class="rounded-corner" src="img/reflection3.jpg" align="center" width="700" height="400" alt="">
+	<br>
+
+	<!-- Masthead Heading -->
+	<!-- Icon Divider -->
+	<div class="divider-custom divider-light">
+		<div class="divider-custom-line"></div>
+		<div class="divider-custom-icon">
+		<i class="fas fa-star"></i>
+		</div>
+		<div class="divider-custom-line"></div>
+	</div>
+
+	<!-- Masthead Subheading -->
+
+	</div>
+	<section class="page-section portfolio" id="portfolio">
+    <div class="container">
+        <p align="justified">In my Junior High School (JHS) years, we had an ICT subject and we did an HTML. I was amazed that time that I was really creating my own webpage but only accessing it through “open with Google Chrome” only. 
+		When Ms. Jen tasks us to create an HTML, I was very excited to do it again. Though, I forgot the lessons way back on JHS, but it is very good to code in HTML again.
+		</p>
+		<p align="justified">
+		I also noticed that some of my classmates were good at designing their website. I realized that it is a part of a challenge that I need to study and learn more about it. I also realized that there are more from the internet when I searched but I did not include it on the website because I did not understand on how the code works. As a part of learning, I wanted to put it on my website when I understand on how it will work on my next website level up activity.
+		Furthermore, I am also curious on how we will deploy it on the internet. Ms. Jen said the steps on Trello on how it will be deployed but the Heroku does not read HTML code. Thankfully, because of Christian, he thought and said to us that there are tricks on deploying our website on Heroku and It worked. 
+		</p>
+		<p> <a href="https://rlormita-lab1.herokuapp.com/index.html">Click here</a> to see my first deployed website. </p>
+	</div>
+	</section>
+  </header>
+  <footer class="footer text-center">
+    <div class="container">
+      <div class="column">
+		
+        <!-- Footer Social Icons -->
+        <div class="col-lg-4 mb-5 mb-lg-0">
+          <h4 class="text-uppercase mb-4">Around the Web</h4>
+          <a class="btn btn-outline-light btn-social mx-1" href="https://b-m.facebook.com/rmlrmt">
+            <i class="fab fa-fw fa-facebook-f"></i>
+          </a>
+          <a class="btn btn-outline-light btn-social mx-1" href="https://twitter.com/rmlrmt">
+            <i class="fab fa-fw fa-twitter"></i>
+          </a>
+          <a class="btn btn-outline-light btn-social mx-1" href="https://ph.linkedin.com/in/rommel-o-29a6ba111">
+            <i class="fab fa-fw fa-linkedin-in"></i>
+          </a>
+          <a class="btn btn-outline-light btn-social mx-1" href="https://www.instagram.com/rmlrmt/">
+            <i class="fab fa-fw fa-dribbble"></i>
+          </a>
+        </div>
+		
+      </div>
+    </div>
+  </footer>
+
+  <!-- Copyright Section -->
+  <section class="copyright py-4 text-center text-white">
+    <div class="container">
+	  <img src="img/mew.gif" width="50" height="50">
+      <small>Copyright &copy; labActivity2 2019</small>
+    </div>
+  </section>
+
+</html>
